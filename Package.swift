035458817977cf36bad3a592b7e99f2371383a39
ResulticksInc <36412134+ResulticksInc@users.noreply.github.com>
2022-03-29 21:12:42 +0530

    Sivakumar: Changed url to run
    Changed url to run

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "REIOSSDK",
-//                      url: "https://mobis.resu.io/mobile_sdk/tata/v1.0.49/REIOSSDK.xcframework.zip",
-                      url: "https://github.com/ResulticksInc/REIOSSDK-SPM/raw/main/REIOSSDK.xcframework.zip",
+                      url: "https://mobis.resu.io/mobile_sdk/tata/v1.0.49/REIOSSDK.xcframework.zip",
+//                      url: "https://github.com/ResulticksInc/REIOSSDK-SPM/raw/main/REIOSSDK.xcframework.zip",
                       checksum: "e139a068b0544b63416e82136e0116d33b0d6dd74c01ff7a3c5846562de2ee32"),
     ]
 )
